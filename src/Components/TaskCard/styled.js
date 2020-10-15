0859ed72545bf20d193f279b88450d055a78b8f5
Vitor R. Bizzi <vitorbizzi5@gmail.com>
2020-10-15 17:18:47 -0300

    Create of Footer and FilterCard

---
+++ src/Components/TaskCard/styled.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+import Styled from 'styled-components';
+
+export const Container = styled.div`
+width: 300%;
+height: 200%;
+
+display: flex;
+align-items: center;
+justify-content: center;
+
+
+`
--- src/Views/Home/index.js
+++ src/Views/Home/index.js
@@ -1,8 +1,44 @@
-import React from 'react';
+//useState ele avisa todos os componentes que certo estado mudou e os outros elementos mudam de cor para se 
+//adaptar para o novo estatado
+import React, {useState} from 'react';
+import * as S from './styles'
+
+//Nossos Componetes
+import Header from '../../Components/Header';
+import Footer from '../../Components/Footer';
+import FilterCard from '../../Components/FilterCard';
+import TaskCard from '../../Components/TaskCard'
 
 function Home() {//O nome da função sempre será o nome da página
-  return <h1>Bem vindo à HOME</h1>
- 
+  const [filteActived, setFilterActived] = useState();
+  return (
+          <S.Container>
+          <Header/>
+          
+          <S.FilterArea>
+          <button type="button"         onClick={() => setFilterActived("all")}> 
+            <FilterCard title="Todos"   actived={filteActived == 'all'} />
+          </button>
+          <button type="button"         onClick={() => setFilterActived("today")}> 
+            <FilterCard title="Hoje"    actived={filteActived == 'today'} />
+          </button>
+          <button type="button"         onClick={() => setFilterActived("week")}> 
+            <FilterCard title="Semana"  actived={filteActived == 'week'} />
+          </button>
+          <button type="button"         onClick={() => setFilterActived("month")}> 
+            <FilterCard title="Mês"     actived={filteActived == 'month'} />
+          </button>
+          <button type="button"         onClick={() => setFilterActived("yaer")}> 
+            <FilterCard title="Ano"     actived={filteActived == 'year'}/>
+          </button>
+          </S.FilterArea>
+           <S.TaskCard>
+             
+           </S.TaskCard>
+          <Footer/>
+
+          </S.Container>
+         ) 
 }
 
 export default Home;
